2d21e816f7b5b9657e335cd39d75670b54fc5bb1
Ovanan <saichandmar111@gmail.com>
2018-02-03 22:15:31 +0530

    ads

--- src/main/java/com/demo/customer/constants/CustomerConstansts.java
+++ src/main/java/com/demo/customer/constants/CustomerConstansts.java
@@ -1,5 +1,5 @@
 package com.demo.customer.constants;
 
 public class CustomerConstansts {
-	public static final String ORDER_SERVICE_CONTEXT_PATH="order-service/order";
+	public static final String ORDER_SERVICE_CONTEXT_PATH="order-service/order-service";
 }
--- src/main/java/com/demo/customer/swagger/SwaggerConfig.java
+++ src/main/java/com/demo/customer/swagger/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.demo.customer.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -20,8 +21,8 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)          
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.demo.customer"))
-				//.paths(PathSelectors.any())
-				.paths(regex("/customer-service.*"))
+				.paths(PathSelectors.any())
+				//.paths(regex("/customer-service.*"))
 				.build()
 				.apiInfo(apiInfo());
 	}
